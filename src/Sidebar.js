b99d091bee87d68b78f6104b8b3144308d6f9ebc
mifouts <fouts.mitch15@gmail.com>
2022-12-28 16:36:08 -0500

    starting sidebar option component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Sidebar.css";
-import HomeIcon from "@mui/icons-material/Home";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <TwitterIcon />
-      <HomeIcon />
     </div>
   );
 }
