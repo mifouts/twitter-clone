8a5c938b74ee80cfa0036bb1a4ec2bee11e25564
mifouts <fouts.mitch15@gmail.com>
2022-12-30 14:56:09 -0500

    adding in rest of sidebar options

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -5,6 +5,11 @@ import SidebarOptions from "./SidebarOptions";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 function Sidebar() {
   return (
@@ -13,6 +18,11 @@ function Sidebar() {
       <SidebarOptions Icon={HomeIcon} text="Home" />
       <SidebarOptions Icon={SearchIcon} text="Explore" />
       <SidebarOptions Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOptions Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOptions Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOptions Icon={ListAltIcon} text="Lists" />
+      <SidebarOptions Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOptions Icon={MoreHorizIcon} text="More" />
     </div>
   );
 }
