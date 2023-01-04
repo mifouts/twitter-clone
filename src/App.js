1826f48e775ee99f51c7a68d36292a50557008bd
mifouts <fouts.mitch15@gmail.com>
2023-01-03 22:53:03 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Widget from "./Components/Widget";
 function App() {
   return (
     <div className="app">
-      <h1>twitter clone</h1>
-
       <Sidebar />
       <Feed />
       <Widget />
