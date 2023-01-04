54015ce67e068ea9f8a27737b5ea7ac4ec9171ed
mifouts <fouts.mitch15@gmail.com>
2023-01-04 15:01:11 -0500

    editing in my picture

--- src/Components/TweetBox.js
+++ src/Components/TweetBox.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./TweetBox.css";
 import { Avatar, Button } from "@mui/material";
+import pfp from "./EportfolioPICTURE.jpg";
 
 function TweetBox() {
   return (
     <div className="tweetBox">
       <form action="">
         <div className="tweetBox__input">
-          <Avatar img="./EportfolioPICTURE.jpg" />
+          <Avatar src={pfp} />
           <input type="text" placeholder="What's Happening?" />
           <input type="text" placeholder="Enter Image URL" />
         </div>
