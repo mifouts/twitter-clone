20f57b4b20c6d972c4417bb7adadd2463748e96a
mifouts <fouts.mitch15@gmail.com>
2023-01-09 15:44:53 -0500

    creating a post

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,12 +2,32 @@ import React from "react";
 import "./Post.css";
 import { Avatar } from "@mui/material";
 import pfp from "./EportfolioPICTURE.jpg";
+import { VerifiedUser } from "@material-ui/icons";
 
 function Post({ displayName, username, verified, text, image, avatar }) {
   return (
     <div className="post">
       <div className="post__avatar">
         <Avatar src={pfp} />
+      </div>
+      <div className="post__body">
+        <div className="post__header">
+          <div className="post__headerText">
+            <h3>
+              Mitch{" "}
+              <span>
+                <VerifiedUser className="post__badge" />
+              </span>
+            </h3>
+          </div>
+          <div className="post__headerDescription">
+            <p>Build a twitter clone!</p>
+          </div>
+        </div>
+        <img
+          src="https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif"
+          alt=""
+        />
       </div>
     </div>
   );
