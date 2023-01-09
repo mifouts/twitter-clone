b74492c60aba070f3d57fcf9a329347a5d054d95
mifouts <fouts.mitch15@gmail.com>
2023-01-09 15:54:02 -0500

    creating post component

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -6,6 +6,7 @@ import { VerifiedUser } from "@material-ui/icons";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import RepeatIcon from "@mui/icons-material/Repeat";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import SendIcon from "@mui/icons-material/Send";
 
 function Post({ displayName, username, verified, text, image, avatar }) {
   return (
@@ -27,11 +28,15 @@ function Post({ displayName, username, verified, text, image, avatar }) {
             <p>Build a twitter clone!</p>
           </div>
         </div>
-        <img src="" alt="" />
+        <img
+          src="https://media.giphy.com/media/Kiy0bxDn36N6LgLMh4/giphy.gif"
+          alt=""
+        />
         <div className="post__footer">
-          <ChatBubbleIcon />
-          <RepeatIcon />
-          <FavoriteBorderIcon />
+          <ChatBubbleIcon fontSize="small" />
+          <RepeatIcon fontSize="small" />
+          <FavoriteBorderIcon fontSize="small" />
+          <SendIcon fontSize="small" />
         </div>
       </div>
     </div>
