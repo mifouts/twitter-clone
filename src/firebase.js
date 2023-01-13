c011987515c386b4136a227b1af32a771664f568
mifouts <fouts.mitch15@gmail.com>
2023-01-13 15:54:48 -0500

    importing firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-_CkrIHZz16eo-sASXh9b-V7tqOa39jY",
+  authDomain: "twitter-clone-79549.firebaseapp.com",
+  projectId: "twitter-clone-79549",
+  storageBucket: "twitter-clone-79549.appspot.com",
+  messagingSenderId: "782050756812",
+  appId: "1:782050756812:web:54828b7b918c152ad7da69",
+  measurementId: "G-RG5R51GM3J",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
