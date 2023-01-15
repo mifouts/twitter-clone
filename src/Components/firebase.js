78a415f5b7d78b1f81ea659f4bc9004a3b7b62c3
mifouts <fouts.mitch15@gmail.com>
2023-01-15 12:30:06 -0500

    getting firebase to work

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-_CkrIHZz16eo-sASXh9b-V7tqOa39jY",
