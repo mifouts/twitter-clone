389b930be549dc03161cad4c09c6e7cbe3603f82
mifouts <fouts.mitch15@gmail.com>
2023-01-25 17:04:36 -0500

    avatar

--- src/Components/TweetBox.js
+++ src/Components/TweetBox.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./TweetBox.css";
 import { Avatar, Button } from "@mui/material";
+import pfp from "./EportfolioPICTURE.jpg";
 
 function TweetBox() {
   return (
     <div className="tweetBox">
       <form action="">
         <div className="tweetBox__input">
-          <Avatar src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fvinicius73.github.io%2Fgravatar-url-generator%2F&psig=AOvVaw3m0YXXoIF4GKFOTxabVOWo&ust=1674770599428000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCICytunc4_wCFQAAAAAdAAAAABAE" />
+          <Avatar src={pfp} />
           <input type="text" placeholder="What's Happening?" />
         </div>
 
